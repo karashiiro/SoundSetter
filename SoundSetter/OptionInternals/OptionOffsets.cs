1109ac9eb400194557131c457d599975bb71cdf5
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-12-17 12:33:51 -0800

    Update options struct offsets for 6.0

--- SoundSetter/OptionInternals/OptionOffsets.cs
+++ SoundSetter/OptionInternals/OptionOffsets.cs
@@ -2,31 +2,31 @@
 {
     public static class OptionOffsets
     {
-        public const int PlayMusicWhenMounted = 44176;
-        public const int EnableNormalBattleMusic = 44192;
-        public const int EnableCityStateBGM = 44208;
-        public const int PlaySystemSounds = 44224;
+        public const int PlayMusicWhenMounted = 44560;
+        public const int EnableNormalBattleMusic = 44576;
+        public const int EnableCityStateBGM = 44592;
+        public const int PlaySystemSounds = 44608;
 
-        public const int MasterVolume = 44256;
-        public const int Bgm = 44272;
-        public const int SoundEffects = 44288;
-        public const int Voice = 44304;
-        public const int SystemSounds = 44320;
-        public const int AmbientSounds = 44336;
-        public const int Performance = 44352;
+        public const int MasterVolume = 44640;
+        public const int Bgm = 44656;
+        public const int SoundEffects = 44672;
+        public const int Voice = 44688;
+        public const int SystemSounds = 44704;
+        public const int AmbientSounds = 44720;
+        public const int Performance = 44736;
 
-        public const int Self = 44368;
-        public const int Party = 44384;
-        public const int OtherPCs = 44400;
+        public const int Self = 44752;
+        public const int Party = 44768;
+        public const int OtherPCs = 44784;
 
-        public const int MasterVolumeMuted = 44416;
-        public const int BgmMuted = 44432;
-        public const int SoundEffectsMuted = 44448;
-        public const int VoiceMuted = 44464;
-        public const int SystemSoundsMuted = 44480;
-        public const int AmbientSoundsMuted = 44496;
-        public const int PerformanceMuted = 44512;
+        public const int MasterVolumeMuted = 44800;
+        public const int BgmMuted = 44816;
+        public const int SoundEffectsMuted = 44832;
+        public const int VoiceMuted = 44848;
+        public const int SystemSoundsMuted = 44864;
+        public const int AmbientSoundsMuted = 44880;
+        public const int PerformanceMuted = 44896;
 
-        public const int EqualizerMode = 44672;
+        public const int EqualizerMode = 45056;
     }
 }
